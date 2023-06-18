1320a284204f0e5468de92d63b5fa3a16ee99a9d
y4asse <yasse0218@icloud.com>
2023-06-18 17:22:39 +0900

    fix romajiShow

--- app/components/playing/gameText.tsx
+++ app/components/playing/gameText.tsx
@@ -11,6 +11,12 @@ const GameText = () => {
   const [, setSituation] = useRecoilState(situationAtom)
   const { hiragana, text } = game
   const { textIndex, totalInput, splitSentence, hiraganaIndex, requiredRomaji } = useTypingLogic(hiragana)
+  const [romajiShow, setRomajiShow] = useState('')
+
+  //表示用のromajiを生成
+  useEffect(() => {
+    setRomajiShow(totalInput + requiredRomaji.join('').substring(totalInput.length))
+  }, [totalInput, requiredRomaji])
 
   //終了したときの処理
   useEffect(() => {
@@ -24,8 +30,16 @@ const GameText = () => {
       <div className="absolute gap-3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 text-white h-full  min-w-full max-w-screen-xl p-6 rounded-2xl flex flex-col justify-center items-center">
         {text[textIndex] && (
           <div>
-            <div className="text-2xl whitespace-nowrap">{text[textIndex].slice(0, 50)}</div>
-            <div className="text-2xl whitespace-nowrap">{text[textIndex].substring(50)}</div>
+            <div className="text-2xl whitespace-nowrap">
+              {text[textIndex].split('').map((char, index) => {
+                return (
+                  <span>
+                    {char}
+                    {index !== 0 && index % 50 === 0 && <br></br>}
+                  </span>
+                )
+              })}
+            </div>
           </div>
         )}
         {/* ひらがなの表示 */}
@@ -41,9 +55,23 @@ const GameText = () => {
             return <span key={index}>{char}</span>
           })}
         </div>
-        <div>
-          <span className="text-gray-500">{totalInput}</span>
-          {requiredRomaji.join('').substring(totalInput.length)}
+        <div className=" whitespace-nowrap ">
+          {romajiShow.split('').map((char, index) => {
+            if (index < totalInput.length) {
+              return (
+                <span className="text-gray-500">
+                  {char}
+                  {index !== 0 && index % 100 === 0 && <br></br>}
+                </span>
+              )
+            }
+            return (
+              <span>
+                {char}
+                {index !== 0 && index % 100 === 0 && <br></br>}
+              </span>
+            )
+          })}
         </div>
         <p></p>
       </div>
