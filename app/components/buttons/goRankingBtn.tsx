e045d97920a6541c83ef09de9286af2414cbe48c
y4asse <yasse0218@icloud.com>
2023-06-23 02:36:41 +0900

    add prefetch false

--- app/components/buttons/goRankingBtn.tsx
+++ app/components/buttons/goRankingBtn.tsx
@@ -7,6 +7,7 @@ const GoRankingBtn = () => {
   return (
     <Link
       href={'/ranking'}
+      prefetch={false}
       className=" text-center w-full  border-black border-4 rounded-xl py-8 hover:bg-black hover:text-white duration-200 transition-all text-2xl font-bold shadow-xl  tracking-widest"
     >
       <GiStairs className="inline-block mr-5" />
