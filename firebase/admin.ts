6235acadc8a259cb0af3701b2389cdf7ad8633ae
y4asse <yasse0218@icloud.com>
2023-07-02 02:03:19 +0900

    fix

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -7,6 +7,8 @@ import { getAuth } from 'firebase-admin/auth'
 //   initializeApp({
 //     credential: cert(serviceAccount)
 //   })
+
+//vercelでは以下の記述で扱う
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID!,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
