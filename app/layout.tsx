084f7754da164132b82dfeea0a03a15ac03c2bed
y4asse <yasse0218@icloud.com>
2023-07-03 11:51:42 +0900

    delete identify

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import Footer from './components/layout/footer'
 import './globals.css'
 import { Sora } from 'next/font/google'
 import GoogleAnalytics from './components/google/googleAnalytics'
-import Identify from './components/device/identify'
 import SessionProvider from './components/sessionProvider/sessionProvider'
 
 const sora = Sora({ subsets: ['latin'] })
@@ -16,18 +15,16 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Identify>
-        <SessionProvider>
-          <RecoilPrivider>
-            <body className={sora.className}>
-              <div className="bg-[url('/img/background.png')] bg-center bg-cover overflow-hidden ">
-                {children}
-                <Footer />
-              </div>
-            </body>
-          </RecoilPrivider>
-        </SessionProvider>
-      </Identify>
+      <SessionProvider>
+        <RecoilPrivider>
+          <body className={sora.className}>
+            <div className="bg-[url('/img/background.png')] bg-center bg-cover overflow-hidden ">
+              {children}
+              <Footer />
+            </div>
+          </body>
+        </RecoilPrivider>
+      </SessionProvider>
     </html>
   )
 }
