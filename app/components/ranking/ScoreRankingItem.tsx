3681da04929e519a6438b6103cb6018d7522a3e3
y4asse <yasse0218@icloud.com>
2023-07-03 15:12:42 +0900

    add hub id

--- app/components/ranking/ScoreRankingItem.tsx
+++ app/components/ranking/ScoreRankingItem.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { Game } from '@/types/game'
 import { format } from 'date-fns'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 type Props = {
@@ -7,10 +10,13 @@ type Props = {
 }
 
 const ScoreRankingItem = ({ game }: Props) => {
+  const router = useRouter()
   return (
     <>
       <td className="pl-10">{game.score}点</td>
-      <td className="pl-10">{game.inputed_thema}</td>
+      <td className="pl-10 cursor-pointer underline" onClick={() => router.push(`/hub/${game.id}`)}>
+        {game.inputed_thema}
+      </td>
       <td>{format(Date.parse(game.created_at), 'yyyy/MM/dd hh:mm')}</td>
     </>
   )
