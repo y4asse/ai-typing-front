399755be97677370761a57af842bd196bb0f14cf
y4asse <yasse0218@icloud.com>
2023-07-05 15:58:47 +0900

    add pagenation

--- app/components/hub/latestGamesList.tsx
+++ app/components/hub/latestGamesList.tsx
@@ -6,18 +6,28 @@ import { useRecoilState } from 'recoil'
 import { hubPageNationOffsetAtom } from '@/recoil/hubPagenationAtom'
 import { getLatestGames } from '@/hooks/getLatestGames'
 import { Game } from '@/types/game'
+import Spinner from '../utils/spinner'
 
 const LatestGamesList = () => {
   const [offset] = useRecoilState(hubPageNationOffsetAtom)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const [games, setGames] = useState<Game[] | null>([])
   useEffect(() => {
+    setIsLoading(true)
     const fetchData = async () => {
       const res = await getLatestGames(offset)
       setGames(res)
     }
-    fetchData()
+    fetchData().then(() => setIsLoading(false))
   }, [offset])
 
+  if (isLoading) {
+    return (
+      <div className="w-full h-full flex justify-center">
+        <Spinner />
+      </div>
+    )
+  }
   if (games == null) {
     return (
       <div className="flex text-3xl font-bold justify-center items-center h-full">
@@ -25,6 +35,7 @@ const LatestGamesList = () => {
       </div>
     )
   }
+
   return (
     <table className="w-full h-full ">
       <tbody>
