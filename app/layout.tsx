4de3a6c2356f8848e9e7ff1549517739252c37f9
y4asse <yasse0218@icloud.com>
2023-07-07 02:21:04 +0900

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,23 +16,28 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      {process.env.NEXT_PUBLIC_GA_ID && (
+        <head>
+          <Script
+            async
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
            window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
  
            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
            `
-        }}
-      />
+            }}
+          />
+        </head>
+      )}
       <SessionProvider>
         <RecoilPrivider>
           <body className={sora.className}>
