08f42ef0f6bbaa74b73e51523cd410108684fdcf
y4asse <yasse0218@icloud.com>
2023-08-09 09:57:19 +0900

    license

--- hooks/getGamesByLike.ts
+++ hooks/getGamesByLike.ts
@@ -1,7 +1,13 @@
 import { LikeCountWithGame } from '@/app/components/hub/gameListByLike'
 
 export const GetGamesByLike = async (offset: number, limit: number) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/likeCountRanking?offset=${offset}&limit=${limit}`)
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/likeCountRanking?offset=${offset}&limit=${limit}`
+  ).catch((err) => {
+    console.log(err)
+    return null
+  })
+  if (res == null) return null
   if (!res.ok) {
     console.log(res.statusText)
     return null
