019e2c6fd3aca9a839816b57bcedade40e8966f1
y4asse <yasse0218@icloud.com>
2023-08-09 15:39:54 +0900

    liked page nation

--- app/components/hub/pageNation.tsx
+++ app/components/hub/pageNation.tsx
@@ -3,9 +3,18 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import React from 'react'
 import { useRecoilState } from 'recoil'
 import { hubPageNationOffsetAtom } from '@/recoil/hubPagenationAtom'
+import { hubTabAtom } from '@/recoil/hubTabAtom'
+import { likeOffsetAtom } from '@/recoil/likeOffset'
 
-const PageNation = ({ totalGameCount }: { totalGameCount: number }) => {
-  const [offset, setOffset] = useRecoilState(hubPageNationOffsetAtom)
+const PageNation = ({
+  totalGameCount,
+  totalLikedGameCount
+}: {
+  totalGameCount: number
+  totalLikedGameCount: number
+}) => {
+  const [tabNumber] = useRecoilState(hubTabAtom)
+  const [offset, setOffset] = useRecoilState(tabNumber == 0 ? hubPageNationOffsetAtom : likeOffsetAtom)
   return (
     <div className="flex text-4xl gap-6 p-3">
       <FaChevronLeft
@@ -24,7 +33,7 @@ const PageNation = ({ totalGameCount }: { totalGameCount: number }) => {
         className=" cursor-pointer"
         onClick={() =>
           setOffset((prev) => {
-            if (prev + 2 > Math.ceil(totalGameCount / 10)) {
+            if (prev + 2 > Math.ceil((tabNumber == 0 ? totalGameCount : totalLikedGameCount) / 10)) {
               return prev
             }
             return prev + 1
